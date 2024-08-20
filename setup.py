deb8ac0a83b6c3714a11000bcc256552c488325a
Aaron C Wacker <30595158+AaronCWacker@users.noreply.github.com>
2024-08-20 18:17:40 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        import nltk
+        nltk.download("popular")
+
+setup(
+    cmdclass={'install': Install},
+    
+    install_requires=[
+      'nltk',
+      ],
+    setup_requires=['nltk']
+    
+   )
